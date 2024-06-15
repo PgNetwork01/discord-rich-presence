43e779d2d4cb31145e33ab39a45b6424b36d89c8
Pg Network <gamerzp780@gmail.com>
2024-06-15 10:33:57 +0530

    Create discord-rich-presence-py.py

---
+++ discord-rich-presence-py.py
@@ -0,0 +1,6 @@
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+    botactivity = discord.Activity(type=discord.ActivityType.watching, name="Dert | /help")
+    await bot.change_presence(activity=botactivity, status=discord.Status.do_not_disturb)
+    await bot.tree.sync()
